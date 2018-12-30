acb97901188e4fa3e6d2c133f476e2ef19cd2cad
Schifer <oaarruda@inf.ufrgs.br>
2018-12-30 11:56:46 -0200

    random colors generator working

--- colorgame.js
+++ colorgame.js
@@ -1,34 +1,79 @@
 /* Variables */
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
-for(i = 0; i < squares.length; i++){
+for (i = 0; i < squares.length; i++) {
     // Add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
     // Always use style.backgroundColor instead of style.background, so it can work in any browser
 
     // Add click listeners to squares
-    squares[i].addEventListener("click", function(){
+    squares[i].addEventListener("click", function () {
         /* alert("Clicked!")
         Grab the color of a clicked square 
         then compare color to picked color
         */
-       var clickedColor = this.style.backgroundColor;
-       if(clickedColor === pickedColor){
-           alert("Correct");
-       }
-       else alert("Wrong")
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            //    alert("Correct");
+            messageDisplay.textContent = "Correct";
+            ChangeColors(clickedColor);
+        }
+        else {
+            //    alert("Wrong");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
     });
 }
+
+function ChangeColors(color) {
+    // loop through all squares
+    // change each color to match given color
+    for (i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    /*
+    1. make Array
+    2. add random rgb colors to array 
+    3. return that array
+    */
+    var arr = []
+    i = 0
+    while (i < num) {
+        arr.push(randomColor());
+        i++;
+    }
+
+    return arr;
+}
+
+function randomColor(){
+    /*
+    pick a red from 0 to 255
+    pick a green from 0 to 255
+    pick a blue from 0 to 255
+    */
+   var red = Math.floor(Math.random() * 256);
+   var green = Math.floor(Math.random() * 256);
+   var blue = Math.floor(Math.random() * 256);
+
+   return "rgb(" + red +"," + green + "," + blue + ")";
+}
+
+
 
